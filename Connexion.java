96e6d07a3c7a1b926cd1d1357dbfef13f0116cb8
MelBeee <mel.beee@deepteal.com>
2015-03-26 19:59:06 -0400

    jnllknkijkjl

--- Connexion.java
+++ Connexion.java
@@ -3,17 +3,14 @@ import java.net.*;
 import java.lang.Object;
 
 public class Connexion implements Runnable
-{
-	public static final int NBCONN = 2;
-	public static int cCourante = 0;
-	public PrintWriter writer;
-	public ServeurEcho uneInstanceDeServeur;
+{	
+	final int MAX_USERNAME = 8;
+	final int MIN_USERNAME = 1;
+	final int MAX_CHAR = 80;
+	final int MIN_CHAR = 0;
+	public static final int NBRE_CONNEXION_MAX = 5;
+	public static int nbreInstanceConnexion = 0;
 	
-	
-	public static final int MAX_USERNAME = 8;
-	public static final int MIN_USERNAME = 1;
-	public static final int MAX_CHAR = 80;
-	public static final int MIN_CHAR = 0;
 	public BufferedReader reader;
 	public Socket unSocket = null; 
 	public String uneLigne = null;
@@ -21,55 +18,51 @@ public class Connexion implements Runnable
 	private	boolean quitter = false; 
 	private	boolean envoyer = true;
 	
-    public Connexion(Socket unSocketUtilise, ServeurEcho unServeur)
+    public Connexion(Socket unSocketUtilise)
     {		
 		unSocket = unSocketUtilise;
-		uneInstanceDeServeur = unServeur;
-		try
-		{
-			writer = new PrintWriter(new OutputStreamWriter(unSocket.getOutputStream()));
-			reader = new BufferedReader(new InputStreamReader(unSocket.getInputStream()));
-		}
-		catch(IOException ioe)
-		{
-			System.err.println(ioe);
-			System.exit(1);
-		}
-		cCourante++;
+		nbreInstanceConnexion++;
     }
 	
     public void run()
     {
 		try
 		{	
-			writer.print("Entrez votre nom d'utilisateur: ");
-			writer.flush();
-         
-			VerifierUsername();
-
-			uneInstanceDeServeur.EcrireDesMessages(username + " viens de joindre la conversation.");
+			reader = new BufferedReader(new InputStreamReader(unSocket.getInputStream()));
+			
+			username = reader.readLine();
+			VerifierLongueurUsername();
+			uneLigne = username + " viens de joindre la conversation.";
+			
 			do
 			{
 				uneLigne = reader.readLine();
-				VerifierLigne();
-				if(envoyer)
-					uneInstanceDeServeur.EcrireDesMessages(username + ": " + uneLigne);
+				VerifierLongueurLigne();
+				
+				if(!uneLigne.isEmpty())
+					uneLigne = username + ": " + uneLigne;
+				else
+					quitter = true;
 					
 			}while(!quitter);
 		}
 		catch(IOException ioe)
 		{
-			//System.err.println("Fermeture innattendue de session sans fermer la connexion");
+			System.err.println("Fermeture innattendue de session sans fermer la connexion.");
 			System.exit(1);
 		}		
+		catch(NullPointerException e)
+        {
+			System.err.println("Fermeture innattendue. Pointeur null rencontre.");
+			System.exit(1);
+        }
 		finally
 		{
 			try
 			{
-				uneInstanceDeServeur.EcrireDesMessages(username + " viens de se deconnecter.");
 				reader.close();
 				unSocket.close();
-				cCourante --;
+				nbreInstanceConnexion --;
 				System.out.println("Client deconnecte");
 			}
 			catch(IOException ioe)
@@ -79,37 +72,20 @@ public class Connexion implements Runnable
 		}
 	}
 	
-	public void VerifierUsername()
+	public void VerifierLongueurUsername()
 	{
-		username = reader.readLine();
 		if(username.length() >  MAX_USERNAME)
 			username = username.substring(0, MAX_USERNAME);
 		else if(username.length() <= MIN_USERNAME)
 			username = unSocket.getInetAddress().getHostAddress();
 	}
 	
-	public void VerifierLigne()
+	public void VerifierLongueurLigne()
 	{
-		envoyer = true;
 		if(uneLigne.length() > MAX_CHAR)
 		{
 			uneLigne = uneLigne.substring(MIN_CHAR, MAX_CHAR);	
 		}
-		else if(uneLigne.isEmpty())
-		{
-			quitter = true;
-			envoyer = false;
-		}
-		else if(uneLigne.trim().length() == MIN_CHAR)
-		{
-			envoyer = false;
-		}
-	}
-	
-	public void EcrireLeMessage(String Message)
-	{
-		writer.println(Message);
-		writer.flush();
 	}
 }
 
--- ServeurEcho.java
+++ ServeurEcho.java
@@ -5,16 +5,73 @@ import java.util.ListIterator;
 
 public class ServeurEcho
 {	
+	final int MIN_CHAR = 0;
+	final int TEMPS_TIMEOUT = 50000;
+	static final int PORT_SOCKET = 50000;
+	PrintWriter writer = null;
+	Socket unSocket = null;
+	ServerSocket socketServeur = null;
 	// un tableau de tout les connexions 
     static ArrayList<Connexion> MesConnexions = new ArrayList<>();
+	String uneLigne = new String(); 
    
    // Methode qui envoit en console le message ecrit par un user a tout les 
    // utilisateurs connectés dans l'array list
-	public static synchronized void EcrireDesMessages(String unMessage)
+	private synchronized void EcrireDesMessages()
 	{
-		for(int cpt = 0 ; cpt < MesConnexions.size() ; cpt++ )
+		if(!MesConnexions.isEmpty())
 		{
-			MesConnexions.get(cpt).EcrireLeMessage(unMessage);
+			for(int cpt = 0 ; cpt < MesConnexions.size() ; cpt++ )
+			{
+				uneLigne = MesConnexions.get(cpt).uneLigne;
+				if(uneLigne.trim().length() != MIN_CHAR)
+				{
+					DistribuerLeMessage();
+					MesConnexions.get(cpt).uneLigne = null;
+				}
+				else if(uneLigne.isEmpty())
+				{
+					uneLigne = MesConnexions.get(cpt).username + "a quitte la conversation.";
+					FermerUneSeuleConnexion(cpt);
+					DistribuerLeMessage();
+				}
+			}
+		}
+	}
+	
+	private synchronized void DistribuerLeMessage()
+	{
+		try
+		{
+			if(uneLigne != null)
+			{
+				for(int cpt = 0 ; cpt < MesConnexions.size() ; cpt++ )
+				{
+					unSocket = MesConnexions.get(cpt).unSocket;
+					writer = new PrintWriter(new OutputStreamWriter(unSocket.getOutputStream()));
+					
+					writer.println(uneLigne);
+					writer.flush();
+				}
+			}
+		}
+		catch(IOException ioe)
+		{
+			
+		}
+	}
+	
+	private void FermerUneSeuleConnexion(int cpt)
+	{
+		try
+		{
+			unSocket = MesConnexions.get(cpt).unSocket;
+			MesConnexions.remove(cpt);
+			unSocket.close();
+		}
+		catch(IOException ioe)
+		{
+			System.err.println("Fermeture innattendue lors d'une tentative de fermeture d'une connexion.");
 		}
 	}
    
@@ -22,9 +79,6 @@ public class ServeurEcho
 	{
 		try
 		{
-			Socket unSocket = null;
-			ServerSocket socketServeur = null;
-			
 			Terminateur unTerminateur = new Terminateur();
 			Thread unDeTerminateur = new Thread(unTerminateur);
 			unDeTerminateur.setDaemon(true);
@@ -38,43 +92,81 @@ public class ServeurEcho
 			{
 				try
 				{
-					if(Connexion.cCourante <= Connexion.NBCONN)
+					if(Connexion.nbreInstanceConnexion <= Connexion.NBRE_CONNEXION_MAX)
 					{
 						unSocket = socketServeur.accept();
-						System.out.println("Connexion du client.");
+	
+						Connexion uneConnexion = new Connexion(unSocket);
+						unSocket.setSoTimeout(TEMPS_TIMEOUT);
 						
-						Connexion uneConnexion = new Connexion(unSocket, this);
 						Thread unDeConnexion = new Thread(uneConnexion);
 						unDeConnexion.setDaemon(true);
-						unDeConnexion.start();
-						
 						MesConnexions.add(uneConnexion);
+						unDeConnexion.start();
+
+						DemanderUsername();
+						System.out.println("Connexion du client.");
+					}
+					else
+					{
+						System.out.println("Le maximum d'utilisateur a ete atteind. Veuillez reessayer plus tard.");
 					}
 				}
-				catch (SocketTimeoutException  e)
+				catch (IOException ioe)
                 {
 				
                 }
+				catch(NullPointerException ioe)
+				{
+					System.err.println("Fermeture innattendue. Pointeur null rencontre.");
+					System.exit(1);
+				}
+				finally
+				{
+					EcrireDesMessages();
+				}
 			}	
-			
-			if(unSocket != null)
-			{
-				unSocket.close();
-				socketServeur.close();
-			}
-		
-			System.exit(1);
+			FermerLesConnexions();
 		}
 		catch(IOException ioe)
 		{
-			System.err.println(ioe);
+			System.err.println("Fermeture innattendue de session sans fermer la connexion.");
 			System.exit(1);
+		}		
+	}
+	
+	private void FermerLesConnexions()
+	{
+		if(!MesConnexions.isEmpty())
+		{
+			for(int cpt = 0; cpt < MesConnexions.size() ; cpt++)
+			{
+				try
+				{
+					unSocket = MesConnexions.get(cpt).unSocket;
+					unSocket.close();
+				}
+				catch(IOException ioe)
+				{
+					System.err.println("Erreur a la fermeture d'une connexion.");
+				}
+				catch(NullPointerException e)
+				{
+					System.err.println("Erreur a la fermeture d'une connexion.");
+				}
+			}
 		}
+	}
+	
+	private void DemanderUsername()
+	{
+		writer.println("Entrez votre nom d'utilisateur: ");
+		writer.flush();
 	}
 
 	public static void main(String args[]) throws IOException
 	{
 		ServeurEcho unBeauServeur = new ServeurEcho();
-		unBeauServeur.lancerServeur(50000);
+		unBeauServeur.lancerServeur(PORT_SOCKET);
 	}
 }
