79efabf95394e97c0153fef8f34fda5af245d0d3
Melissa <201029426@P104-16.clg.priv>
2015-04-10 15:07:13 -0400

    Push des nouveaux shits

--- Connexion.java
+++ Connexion.java
@@ -1,86 +1,149 @@
+// Connexion.java
+// Fait par Melissa Boucher et Francis Thibodeau 
+// 27 mars 2015 
+// Collège Lionel Groulx
+
 import  java.io.*;
 import java.net.*;
 import java.lang.Object;
 
 public class Connexion implements Runnable
-{	
-	final int MAX_USERNAME = 8;
-	final int MIN_USERNAME = 1;
-	final int MAX_CHAR = 80;
-	final int MIN_CHAR = 0;
+{
+	// nbre de connexion maximale 
+	public static final int NBCONN = 4;
+	// nbre de connexion présentement utilisé
+	public static int cCourante = 0;
+	// variable utilisé pour stocké ce que l'utilisateur veux écrire
+	private String uneLigne = null;
+	// username de l'instance de la connexion
+	private String username = null;
+	// variable constante pour les limites du username et d'une ligne
+	private static final int MAX_USERNAME = 8;
+	private static final int MIN_USERNAME = 1;
+	private static final int MAX_CHAR = 80;
+	private static final int MIN_CHAR = 0;
+	// writer et reader qui permettre d'ecrire ou de lire en console
+	private BufferedReader reader;
+	private PrintWriter writer;
+	// socket de l'utilisateur
+	private Socket unSocket = null; 
+	private ServeurEcho uneInstanceDeServeur;
+	// est-ce qu'on quitte ou non
+	boolean quitter = false; 
+	// est-ce qu'on envoye ou non
+	boolean envoyer = true;
 	
-	public BufferedReader reader;
-	public Socket unSocket = null; 
-	public String uneLigne = null;
-	public String username = null;
-	private	boolean quitter = false; 
-	private	boolean envoyer = true;
-	
-    public Connexion(Socket unSocketUtilise)
-    {		
+	//---- Constructeur de Connexion ----\\
+	// prend en parametere un socket et une instance de ServeurEcho pour avoir accès au méthode de cette derniere.
+    public Connexion(Socket unSocketUtilise, ServeurEcho unServeur)
+    {	
+		// affectation des variables
 		unSocket = unSocketUtilise;
+		uneInstanceDeServeur = unServeur;
+		try
+		{
+			writer = new PrintWriter(new OutputStreamWriter(unSocket.getOutputStream()));
+			reader = new BufferedReader(new InputStreamReader(unSocket.getInputStream()));
+		}
+		catch(IOException ioe)
+		{
+			System.err.println(ioe);
+			System.exit(1);
+		}
+		// on augmente le nombre d'instance de connexion
+		cCourante++;
     }
 	
+	//---- Verification du username ----\\
+	// Sert a verifier que notre username est correctement entré. 
+	private void VerifierUsername()
+	{
+		if(username.length() >  MAX_USERNAME) // si ca depasse la limite, on tronque
+			username = username.substring(0, MAX_USERNAME);
+		else if(username.length() <= MIN_USERNAME) // si c'est sous la limite, on utilise l'adresse ip
+			username = unSocket.getInetAddress().getHostAddress(); 
+	}
+	
+	//---- Verification du message ----\\
+	// Sert a verifier que notre message est correctement ecrit
+	private void VerifierLigne()
+	{
+		envoyer = true;
+		if(uneLigne.length() > MAX_CHAR) // si ca depasse la limite, on tronque
+		{
+			uneLigne = uneLigne.substring(MIN_CHAR, MAX_CHAR);	
+		}
+		else if(uneLigne.isEmpty()) // si l'utilisateur entre une ligne vide, ca veux dire qu'il veux quitter
+		{
+			quitter = true; // on veux quitter
+			envoyer = false; // on envoit pas 
+		}
+		else if(uneLigne.trim().length() == MIN_CHAR) // si une ligne est composé d'espace seulement on envoit pas
+		{
+			envoyer = false; // on envoit pas
+		}
+	}
+	
+	//---- Ecrire le message ----\\
+	// Écrit le message envoyer pas un autre utilisateur dans la console de putty (dans ce cas ci)
+	public void EcrireLeMessage(String Message)
+	{
+		writer.println(Message);
+		writer.flush();
+	}
+
     public void run()
     {
 		try
 		{	
-			reader = new BufferedReader(new InputStreamReader(unSocket.getInputStream()));
+			//demande du nom d'utilisateur
+			writer.print("Entrez votre nom d'utilisateur: ");
+			writer.flush();
 			
-			username = reader.readLine();
-			VerifierLongueurUsername();
-			uneLigne = username + " viens de joindre la conversation.";
+		 	username = reader.readLine();
+			VerifierUsername();
 			
-			do
+			// message d'acceuil dans le channel
+			uneInstanceDeServeur.EcrireDesMessages(username + " viens de joindre la conversation.");
+			
+			do // tant que l'utilisateur ne quitte pas, on attend qu'il écrit un message
 			{
+				// get le message
 				uneLigne = reader.readLine();
-				VerifierLongueurLigne();
+				VerifierLigne(); // verifie le message 
 				
-				if(!uneLigne.isEmpty())
-					uneLigne = username + ": " + uneLigne;
-				else
-					quitter = true;
+				if(envoyer)  // si il est correctement écrit on l'envoit 
+					uneInstanceDeServeur.EcrireDesMessages(username + ": " + uneLigne);
 					
 			}while(!quitter);
 		}
 		catch(IOException ioe)
 		{
-			System.err.println("Fermeture innattendue de session sans fermer la connexion.");
-			System.exit(1);
+
 		}		
-		catch(NullPointerException e)
-        {
-			System.err.println("Fermeture innattendue. Pointeur null rencontre.");
-			System.exit(1);
-        }
+		catch(NullPointerException ioe)
+		{
+			
+		}
 		finally
 		{
 			try
 			{
+				// message de depart d'un utilisateur dans le chat
+				uneInstanceDeServeur.EcrireDesMessages(username + " viens de se deconnecter.");
+				// fermeture des writer/reader et socket
 				reader.close();
+				writer.close();
 				unSocket.close();
+				// decremente le nombre d'instance de connexion
+				cCourante --;
+				// affichage dans la console serveur qu'un client s'est déconnecté
 				System.out.println("Client deconnecte");
 			}
 			catch(IOException ioe)
 			{ 
 				
 			}
-		}
-	}
-	
-	public void VerifierLongueurUsername()
-	{
-		if(username.length() >  MAX_USERNAME)
-			username = username.substring(0, MAX_USERNAME);
-		else if(username.length() <= MIN_USERNAME)
-			username = unSocket.getInetAddress().getHostAddress();
-	}
-	
-	public void VerifierLongueurLigne()
-	{
-		if(uneLigne.length() > MAX_CHAR)
-		{
-			uneLigne = uneLigne.substring(MIN_CHAR, MAX_CHAR);	
 		}
 	}
 }
--- ServeurEcho.java
+++ ServeurEcho.java
@@ -1,3 +1,8 @@
+// ServeurEcho.java
+// Fait par Melissa Boucher et Francis Thibodeau 
+// 27 mars 2015 
+// Collège Lionel Groulx
+
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
@@ -5,168 +10,89 @@ import java.util.ListIterator;
 
 public class ServeurEcho
 {	
-	final int MIN_CHAR = 0;
-	final int TEMPS_TIMEOUT = 50000;
-	static final int PORT_SOCKET = 50000;
-	final int NBRE_CONNEXION_MAX = 5;
-	PrintWriter writer = null;
-	Socket unSocket = null;
-	ServerSocket socketServeur = null;
 	// un tableau de tout les connexions 
     static ArrayList<Connexion> MesConnexions = new ArrayList<>();
-	String uneLigne = new String(); 
+	// port utilisé pour la connexion au serveur
+	static final int PORT = 50000;
+	// temps avant que le serveur kick un user
+	final int TEMPS_TIMEOUT = 90000;
    
    // Methode qui envoit en console le message ecrit par un user a tout les 
    // utilisateurs connectés dans l'array list
-	private synchronized void EcrireDesMessages()
+	public static synchronized void EcrireDesMessages(String unMessage)
 	{
-		if(!MesConnexions.isEmpty())
+		for(int cpt = 0 ; cpt < MesConnexions.size() ; cpt++ )
 		{
-			for(int cpt = 0 ; cpt < MesConnexions.size() ; cpt++ )
-			{
-				uneLigne = MesConnexions.get(cpt).uneLigne;
-				if(uneLigne.trim().length() != MIN_CHAR)
-				{
-					DistribuerLeMessage();
-					MesConnexions.get(cpt).uneLigne = null;
-				}
-				else if(uneLigne.isEmpty())
-				{
-					uneLigne = MesConnexions.get(cpt).username + "a quitte la conversation.";
-					FermerUneSeuleConnexion(cpt);
-					DistribuerLeMessage();
-				}
-			}
-		}
-	}
-	
-	private synchronized void DistribuerLeMessage()
-	{
-		try
-		{
-			if(uneLigne != null)
-			{
-				for(int cpt = 0 ; cpt < MesConnexions.size() ; cpt++ )
-				{
-					unSocket = MesConnexions.get(cpt).unSocket;
-					writer = new PrintWriter(new OutputStreamWriter(unSocket.getOutputStream()));
-					
-					writer.println(uneLigne);
-					writer.flush();
-				}
-			}
-		}
-		catch(IOException ioe)
-		{
-			
-		}
-	}
-	
-	private void FermerUneSeuleConnexion(int cpt)
-	{
-		try
-		{
-			unSocket = MesConnexions.get(cpt).unSocket;
-			MesConnexions.remove(cpt);
-			unSocket.close();
-		}
-		catch(IOException ioe)
-		{
-			System.err.println("Fermeture innattendue lors d'une tentative de fermeture d'une connexion.");
+			MesConnexions.get(cpt).EcrireLeMessage(unMessage);
 		}
 	}
    
+   // Methode qui fait pas mal tout 
 	public void lancerServeur(int port)
 	{
 		try
 		{
+			// Creation des sockets 
+			Socket unSocket = null;
+			ServerSocket socketServeur = null;
+			
+			// creation et initialisation et affectation du thread de serveur
 			Terminateur unTerminateur = new Terminateur();
 			Thread unDeTerminateur = new Thread(unTerminateur);
 			unDeTerminateur.setDaemon(true);
 			unDeTerminateur.start();
 			
+			// affectation du socket de serveur
 			socketServeur = new ServerSocket(port);
 			socketServeur.setSoTimeout(1000);
 			
-			
+			// tant et aussi longtemps que le thread serveur est vivant 
 			while(unDeTerminateur.isAlive())
 			{
 				try
 				{
-					if(MesConnexions.size() + 1 <= NBRE_CONNEXION_MAX)
-					{	
+				// si le nombre maximal de connexion n'est pas atteinte
+					if(Connexion.cCourante <= Connexion.NBCONN)
+					{
+					// on accepte un nouveau socket (un nouveau client)
 						unSocket = socketServeur.accept();
-						Connexion uneConnexion = new Connexion(unSocket);
 						unSocket.setSoTimeout(TEMPS_TIMEOUT);
+						System.out.println("Connexion du client.");
 						
+					// on créer une instance de connexion associé a ce socket
+						Connexion uneConnexion = new Connexion(unSocket, this);
 						Thread unDeConnexion = new Thread(uneConnexion);
 						unDeConnexion.setDaemon(true);
 						unDeConnexion.start();
+						
+					//on l'ajoute a notre array de connexion
 						MesConnexions.add(uneConnexion);
-
-						//DemanderUsername();
-						System.out.println("Connexion du client.");
-					}
-					else
-					{
-						System.out.println("Le maximum d'utilisateur a ete atteind. Veuillez reessayer plus tard.");
 					}
 				}
-				catch (IOException ioe)
+				catch (SocketTimeoutException  e)
                 {
 				
                 }
-				catch(NullPointerException ioe)
-				{
-					System.err.println("Fermeture innattendue. Pointeur null rencontre.");
-					System.exit(1);
-				}
-				finally
-				{
-					EcrireDesMessages();
-				}
 			}	
-			FermerLesConnexions();
+			// on ferme nos sockets
+			if(unSocket != null)
+			{
+				unSocket.close();
+				socketServeur.close();
+			}
+		
+			System.exit(1);
 		}
 		catch(IOException ioe)
 		{
-			System.err.println("Fermeture innattendue de session sans fermer la connexion.");
+			System.err.println(ioe);
 			System.exit(1);
-		}		
-	}
-	
-	private void FermerLesConnexions()
-	{
-		if(!MesConnexions.isEmpty())
-		{
-			for(int cpt = 0; cpt < MesConnexions.size() ; cpt++)
-			{
-				try
-				{
-					unSocket = MesConnexions.get(cpt).unSocket;
-					unSocket.close();
-				}
-				catch(IOException ioe)
-				{
-					System.err.println("Erreur a la fermeture d'une connexion.");
-				}
-				catch(NullPointerException e)
-				{
-					System.err.println("Erreur a la fermeture d'une connexion.");
-				}
-			}
 		}
 	}
 	
-	private void DemanderUsername()
-	{
-		writer.println("Entrez votre nom d'utilisateur: ");
-		writer.flush();
-	}
-
+	// main qui appele lancerServeur et c'est pas mal ca
 	public static void main(String args[]) throws IOException
 	{
-		ServeurEcho unBeauServeur = new ServeurEcho();
-		unBeauServeur.lancerServeur(PORT_SOCKET);
+		new ServeurEcho().lancerServeur(PORT);
 	}
 }
